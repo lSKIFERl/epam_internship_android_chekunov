d7cc874622aafcfead7c732d6d3bc7f5f28c2a11
Chekunov Roman <skiferion@gmail.com>
2021-06-07 23:03:02 +0300

    [lection_8] Creating simple app

---
+++ app/src/main/java/com/skifer/epam_internship_android_checkunov/Amogus.kt
@@ -0,0 +1,11 @@
+package com.skifer.epam_internship_android_checkunov
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Amogus: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.amogus)
+    }
+}
