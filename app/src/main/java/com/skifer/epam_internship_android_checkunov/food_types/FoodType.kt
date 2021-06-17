dd7d88e43686aa606a900d9bfe9d05d418794ffe
Chekunov Roman <skiferion@gmail.com>
2021-06-17 15:38:23 +0300

    [lection_10] Added mealModel and some enums for food types

---
+++ app/src/main/java/com/skifer/epam_internship_android_checkunov/food_types/FoodType.kt
@@ -0,0 +1,12 @@
+package com.skifer.epam_internship_android_checkunov.food_types
+
+enum class FoodType(val title: String?){
+
+    SOUP("Суп"),
+    MEAT("Мясо"),
+    FISH("Рыба"),
+    MILK("Молочное"),
+    BAKERY("Выпечка"),
+    DESSERTS("Десерты");
+
+}
---
+++ app/src/main/java/com/skifer/epam_internship_android_checkunov/food_types/KitchenCountry.kt
@@ -0,0 +1,15 @@
+package com.skifer.epam_internship_android_checkunov.food_types
+
+enum class KitchenCountry (val country: String) {
+    ITALIAN("Итальянская"),
+    FRENCH("Французская"),
+    GERMAN("Немецкая"),
+    MEXICAN("Мексиканская"),
+    PAN_ASIAN("Паназиатская"),
+    EAST("Восточная"),
+    UKRAINIAN("Украинская"),
+    RUSSIAN("Русская"),
+    USA("Американская"),
+    UK("Британская"),
+    JAMAICAN("Ямайская")
+}
